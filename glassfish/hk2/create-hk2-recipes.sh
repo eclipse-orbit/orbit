3172db1e505885f5ff0f4c2bcf488d84544c25f9
Roland Grunberg <rgrunber@redhat.com>
2016-09-07 16:31:06 -0400

    Fix Jersey / HK2 stack and publish to repository.
    - org.glassfish.hk2.api imports classes from org.aopalliance
    - org.glassfish.hk2.locator imports classes from javassist.util.proxy
      and other common imports of other hk2 packages
    - org.glassfish.jersey.core.jersey-common, and
      org.glassfish.hk2.osgi-resource-locator are the Bundle-SymbolicName
      used by upstream so we should preserve them
    - org.glassfish.hk2.osgi-resource-locator should match upstream
      versioning
    
    Change-Id: Ia93e641a6030cd20d770545dcb770103183faa85

--- glassfish/hk2/create-hk2-recipes.sh
+++ glassfish/hk2/create-hk2-recipes.sh
@@ -5,7 +5,7 @@ mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.hk2 -DartifactId=hk2-api 
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.hk2 -DartifactId=hk2-locator -DbundleSymbolicName=org.glassfish.hk2.locator "$@"
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.hk2 -DartifactId=hk2-utils -DbundleSymbolicName=org.glassfish.hk2.utils "$@"
 
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.hk2 -DartifactId=osgi-resource-locator -DbundleSymbolicName=org.glassfish.hk2.osgiresourcelocator "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.hk2 -DartifactId=osgi-resource-locator -DbundleSymbolicName=org.glassfish.hk2.osgi-resource-locator "$@"
 
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.hk2.external -DartifactId=asm-all-repackaged -DbundleSymbolicName=org.glassfish.hk2.external.asm "$@"
 
--- glassfish/jersey/create-jersey-recipes.sh
+++ glassfish/jersey/create-jersey-recipes.sh
@@ -2,9 +2,9 @@
 set -euv
 
 # core
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.core -DartifactId=jersey-common -DbundleSymbolicName=org.glassfish.jersey.core.common "$@"
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.core -DartifactId=jersey-client -DbundleSymbolicName=org.glassfish.jersey.core.client "$@"
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.core -DartifactId=jersey-server -DbundleSymbolicName=org.glassfish.jersey.core.server "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.core -DartifactId=jersey-common -DbundleSymbolicName=org.glassfish.jersey.core.jersey-common "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.core -DartifactId=jersey-client -DbundleSymbolicName=org.glassfish.jersey.core.jersey-client "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.core -DartifactId=jersey-server -DbundleSymbolicName=org.glassfish.jersey.core.jersey-server "$@"
 
 # containers
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.containers -DartifactId=jersey-container-servlet-core -DbundleSymbolicName=org.glassfish.jersey.containers.servlet.core "$@"
@@ -12,7 +12,7 @@ mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.containers -Dartif
 
 # client connectors
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.connectors -DartifactId=jersey-jetty-connector -DbundleSymbolicName=org.glassfish.jersey.connectors.jetty "$@"
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.connectors -DartifactId=jersey-apache-connector -DbundleSymbolicName=org.glassfish.jersey.connectors.apache "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.connectors -DartifactId=jersey-apache-connector -DbundleSymbolicName=org.glassfish.jersey.apache.connector "$@"
 
 # extensions
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.ext -DartifactId=jersey-entity-filtering -DbundleSymbolicName=org.glassfish.jersey.ext.entityfiltering "$@"
@@ -23,7 +23,7 @@ mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-sse -DbundleSymbolicName=org.glassfish.jersey.media.sse "$@"
 
 # other
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles.repackaged -DartifactId=jersey-guava -DbundleSymbolicName=org.glassfish.jersey.repackaged.guava "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles.repackaged -DartifactId=jersey-guava -DbundleSymbolicName=org.glassfish.jersey.bundles.repackaged.jersey-guava "$@"
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles.repackaged -DartifactId=jersey-jsr166e -DbundleSymbolicName=org.glassfish.jersey.repackaged.jsr166e "$@"
 
 # all-in-one
