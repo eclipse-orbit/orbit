bdeee4a5b32f3bddebd1ad552040d1f493817759
Steffen Pingel <steffen.pingel@tasktop.com>
2017-02-08 17:15:45 +0100

    add script for creating Apache Orbit bundles

---
+++ apache/create-apache-recipes.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+function ebr () {
+  mvn ebr:create-recipe -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dclassifier=$4 -DbundleSymbolicName=$5
+}
+
+# Edit these as needed
+GID='commons-codec'
+VERSIONS=(
+1.9
+)
+ARTIDS=(
+commons-codec
+)
+BSNS=(
+commons-codec
+)
+
+for (( i=0; i< ${#ARTIDS[@]}; i++ )); do
+  ebr ${GID} ${ARTIDS[${i}]} ${VERSIONS[${i}]} "${CLASSIFIERS[${i}]}" ${BSNS[${i}]}
+done
