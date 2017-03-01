e2c7c60d60771931a087b26584d12004acc71fe0
Sarika Sinha <sarika.sinha@in.ibm.com>
2017-03-01 13:39:23 +0530

    Bug 499717 - Update to Ant 1.10.1
    Change-Id: If5e6a688415be3454be942c0c43cf7b6a49f1fb5
    Signed-off-by: Sarika Sinha <sarika.sinha@in.ibm.com>
    Signed-off-by: Gunnar Wagenknecht <gunnar@wagenknecht.org>
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

---
+++ apache/ant/create-ant-recipes.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+function ebr () {
+  mvn ebr:create-recipe -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -DbundleSymbolicName=$4
+}
+
+# Edit these as needed
+GID='org.apache.ant'
+VERSION='1.10.1'
+ARTIDS=(
+ant
+)
+BSNS=(
+org.apache.ant
+)
+
+for (( i=0; i< ${#ARTIDS[@]}; i++ )); do
+  ebr ${GID} ${ARTIDS[${i}]} ${VERSION} ${BSNS[${i}]}
+done
