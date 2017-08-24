4db369c373c9ce398c873d4f0dece530f63c75b3
Noopur Gupta <noopur_gupta@in.ibm.com>
2017-08-24 12:11:10 +0530

    Bug 517063: [JUnit 5] Add JUnit 5 bundles to Orbit
    JUnit 5.0.0-RC3
    
    Change-Id: I4be6ed65f3e53dba7116ac85b56d92daa7b6931f

--- junit/junit5/create-junit5-recipes.sh
+++ junit/junit5/create-junit5-recipes.sh
@@ -6,7 +6,7 @@ function ebr () {
 
 # Edit these as needed for 'org.junit.platform'
 GID_PLATFORM='org.junit.platform'
-VERSION_PLATFORM='1.0.0-RC2'
+VERSION_PLATFORM='1.0.0-RC3'
 ARTIDS_PLATFORM=(
 junit-platform-commons
 junit-platform-engine
@@ -28,7 +28,7 @@ done
 
 # Edit these as needed for 'org.junit.jupiter'
 GID_JUPITER='org.junit.jupiter'
-VERSION_JUPITER='5.0.0-RC2'
+VERSION_JUPITER='5.0.0-RC3'
 ARTIDS_JUPITER=(
 junit-jupiter-api
 junit-jupiter-engine
@@ -48,7 +48,7 @@ done
 
 # Edit these as needed for 'org.junit.vintage'
 GID_VINTAGE='org.junit.vintage'
-VERSION_VINTAGE='4.12.0-RC2'
+VERSION_VINTAGE='4.12.0-RC3'
 ARTIDS_VINTAGE=(
 junit-vintage-engine
 )
