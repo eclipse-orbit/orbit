ac627940284818f975a65aa3b9adfcfdd617af2f
Tony Homer <tony.homer@intel.com>
2020-04-22 09:14:35 -0700

    bug 558284 - add org.immutables.value 2.8.3
    CQ: 21974
    Change-Id: Id36cd5b17d9fbe2e9bf52d74ed80872da0b251aa
    Signed-off-by: Tony Homer <tony.homer@intel.com>

---
+++ immutables/create-immutables-recipes.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+function ebr () {
+  mvn ebr:create-recipe -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -DbundleSymbolicName=$4
+}
+
+# Edit these as needed
+GIDS=(
+org.immutables
+)
+VERSION='2.8.3'
+ARTIDS=(
+value
+)
+BSNS=(
+org.immutables.value
+)
+
+for (( i=0; i< ${#ARTIDS[@]}; i++ )); do
+  ebr ${GIDS[${i}]} ${ARTIDS[${i}]} ${VERSION} ${BSNS[${i}]}
+done
