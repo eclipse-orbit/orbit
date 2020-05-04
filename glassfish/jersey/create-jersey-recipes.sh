ebc631b5ffc83f19c8c2cd7157c6d1b49741a4d2
Tony Homer <tony.homer@intel.com>
2020-05-03 23:25:45 -0700

    bug 558284 - update jersey to 2.30.1
    org.glassfish.jersey.bundles.repackaged.jersey-guava is not present in 2.30.1
    org.glassfish.jersey.repackaged.jsr166e is not present in 2.30.1
    add jersey-hk2, see jersey 2.26 release notes:
    https://eclipse-ee4j.github.io/jersey.github.io/release-notes/2.26.html
    
    Change-Id: Ic23923b95bc60c7d0cb125c5b798d4adf91ac35f
    Signed-off-by: Tony Homer <tony.homer@intel.com>

--- glassfish/jersey/create-jersey-recipes.sh
+++ glassfish/jersey/create-jersey-recipes.sh
@@ -11,20 +11,20 @@ mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.containers -Dartif
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.containers -DartifactId=jersey-container-servlet -DbundleSymbolicName=org.glassfish.jersey.containers.servlet "$@"
 
 # client connectors
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.connectors -DartifactId=jersey-jetty-connector -DbundleSymbolicName=org.glassfish.jersey.connectors.jetty "$@"
+#mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.connectors -DartifactId=jersey-jetty-connector -DbundleSymbolicName=org.glassfish.jersey.connectors.jetty "$@"
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.connectors -DartifactId=jersey-apache-connector -DbundleSymbolicName=org.glassfish.jersey.apache.connector "$@"
 
 # extensions
 mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.ext -DartifactId=jersey-entity-filtering -DbundleSymbolicName=org.glassfish.jersey.ext.entityfiltering "$@"
 
 # media support
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-multipart -DbundleSymbolicName=org.glassfish.jersey.media.multipart "$@"
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-moxy -DbundleSymbolicName=org.glassfish.jersey.media.moxy "$@"
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-sse -DbundleSymbolicName=org.glassfish.jersey.media.sse "$@"
-
-# other
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles.repackaged -DartifactId=jersey-guava -DbundleSymbolicName=org.glassfish.jersey.bundles.repackaged.jersey-guava "$@"
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles.repackaged -DartifactId=jersey-jsr166e -DbundleSymbolicName=org.glassfish.jersey.repackaged.jsr166e "$@"
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-json-jackson -DbundleSymbolicName=org.glassfish.jersey.media.jersey-media-json-jackson "$@"
+#mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-multipart -DbundleSymbolicName=org.glassfish.jersey.media.multipart "$@"
+#mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-moxy -DbundleSymbolicName=org.glassfish.jersey.media.moxy "$@"
+#mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.media -DartifactId=jersey-media-sse -DbundleSymbolicName=org.glassfish.jersey.media.sse "$@"
 
 # all-in-one
-mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles -DartifactId=jaxrs-ri -DbundleSymbolicName=org.glassfish.jersey.bundles.jaxrs.ri "$@"
+#mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.bundles -DartifactId=jaxrs-ri -DbundleSymbolicName=org.glassfish.jersey.bundles.jaxrs.ri "$@"
+
+# inject
+mvn -U -e -V ebr:create-recipe -DgroupId=org.glassfish.jersey.inject -DartifactId=jersey-hk2 -DbundleSymbolicName=org.glassfish.jersey.inject.jersey-hk2 "$@"
