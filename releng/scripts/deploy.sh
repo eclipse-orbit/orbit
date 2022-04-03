0ea490688d95e85df8bdad78f0555fac5155ddf7
Jonah Graham <jonah@kichwacoders.com>
2022-04-03 18:42:59 -0400

    Bug 568936: Fix typos/syntax error in build scripts
    Change-Id: Ic453c7ea2d71f9b7e48a6789c7638185ba30c4d3

--- releng/scripts/deploy.sh
+++ releng/scripts/deploy.sh
@@ -108,7 +108,7 @@ if [ -n "${SIMREL_NAME}" ]; then
   upload_composite_repo_files ${NEW_BUILD_LABEL} ../drops/${NEW_BUILD_LABEL}/repository ${SIMREL_NAME}
 fi
 
-if [ -n "${DESCRIPTION}"]; then
+if [ -n "${DESCRIPTION}" ]; then
   scp genie.orbit@projects-storage.eclipse.org:${ORBIT_DOWNLOAD_LOC}/notes.php notes.php
   sed -i '/Intentionally not php-closed, since included from PHP section/ i $notes["'${NEW_BUILD_LABEL}'"]="'"${DESCRIPTION}"'";' notes.php
   scp notes.php genie.orbit@projects-storage.eclipse.org:${ORBIT_DOWNLOAD_LOC}/notes.php
